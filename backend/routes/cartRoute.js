7f0f5e5be4eff3bb689ab87a7cc9efaa80b88ce2
Rayudu96408 <kungfupandakabaap@gmail.com>
2024-08-03 12:13:24 +0530

    Update cartRoute.js

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -5,6 +5,7 @@ import authMiddleware from '../middleware/auth.js';
 const cartRouter = express.Router();
 
 cartRouter.post("/get",authMiddleware,getCart);
+
 cartRouter.post("/add",authMiddleware,addToCart);
 cartRouter.post("/remove",authMiddleware,removeFromCart);
 
